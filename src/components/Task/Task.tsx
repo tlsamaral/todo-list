1769fe5c39c379a2b5eb7e882ff3a7d948a0309d
talles.amaral <worktalles@hotmail.com>
2024-11-19 14:22:25 -0300

    feat: task done controle

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,13 +1,36 @@
-import { Trash } from "lucide-react";
+import { Check, Trash } from "lucide-react";
 
 import styles from './Task.module.css'
 
-export function TaskItem() {
+export interface Task {
+    id: number
+    title: string
+    isDone: boolean
+}
+interface TaskItemProps { 
+    task: Task
+    onDone: ({ id, value }: { id: number, value: boolean }) => void
+    onDelete: (id: number) => void
+}
+export function TaskItem({ task, onDone, onDelete }: TaskItemProps) {
+
+    function taskToggleDone() {
+        onDone({ id: task.id, value: !task.isDone })
+    }
+
+    function handleDeleteTask() {
+        onDelete(task.id)
+    }
+
+    const taskClass = task.isDone ? styles.checkboxChecked : styles.checkbox
     return (
         <div className={styles.task}>
-            <input type="checkbox" />
-            <span>Estudar React</span>
-            <button><Trash /></button>
+            <label className={taskClass} htmlFor={`task-check-${task.id}`}>
+                <input type="checkbox" id={`task-check-${task.id}`} checked={task.isDone} onChange={() => taskToggleDone()} />
+                <Check size={14} />
+            </label>
+            <span>{task.title}</span>
+            <button onClick={() => handleDeleteTask()}><Trash size={16} /></button>
         </div>
     )
 }
